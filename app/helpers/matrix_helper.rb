7ed2349bc4e9e0fd5451215173c5820d2ee98643
Steven Anderson <steve@whilefalse.net>
2011-03-29 17:15:57 +0100

    Stuff

--- app/helpers/matrix_helper.rb
+++ app/helpers/matrix_helper.rb
@@ -1,12 +1,15 @@
 module MatrixHelper
   CHART_URL = "https://chart.googleapis.com/chart?cht=tx&chl="
 
-  def matrix_latex matrix
-    tex = '\begin{bmatrix}' + matrix.to_a.map {|row| row.join(' & ')}.join(' \\\\') + '\end{bmatrix}'
-    image_tag tex_url(tex), :alt => 'Matrix Preview'
+  def tex_for_matrix matrix, type='b'
+    '\begin{'+type+'matrix}' + matrix.to_a.map {|row| row.join(' & ')}.join(' \\\\') + '\end{'+type+'matrix}'
   end
 
-  def tex_url tex
+  def tex_image tex, alt='Matrix Preview'
+    image_tag chart_url(tex), :alt => alt
+  end
+
+  def chart_url tex
     CHART_URL + u(tex)
   end
 end
