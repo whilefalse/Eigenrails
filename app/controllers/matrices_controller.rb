1001dc80602f4213685e84b2965e1e5c7ef91b6d
Steven Anderson <steve@whilefalse.net>
2011-03-29 09:02:25 +0100

    test

--- app/controllers/matrices_controller.rb
+++ app/controllers/matrices_controller.rb
@@ -4,6 +4,7 @@ class MatricesController < ApplicationController
   def index
     @dimension = 2
     @matrix = Matrix[*params[:matrix].values.map {|row| row.values.map {|elem| elem.to_i}}] if params[:matrix]
+    @params = params
   end
 
 end
