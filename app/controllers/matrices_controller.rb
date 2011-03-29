7b0f3dc6253fa33f7fb608dbb7bca8dde52be3b2
Steven Anderson <steve@whilefalse.net>
2011-03-29 09:56:17 +0100

    configurable size

--- app/controllers/matrices_controller.rb
+++ app/controllers/matrices_controller.rb
@@ -2,13 +2,7 @@ require "matrix"
 class MatricesController < ApplicationController
 
   def index
-    @dimension = if params[:size]
-                   params[:size].to_i
-                 elsif params[:matrix]
-                   params[:matrix].length
-                 else
-                   4
-                 end
+    @dimension = if params[:size] then params[:size].to_i elsif params[:matrix] then params[:matrix].length else 4 end
     @matrix = Matrix[*params[:matrix].values.map {|row| row.values.map {|elem| elem.to_i}}] if params[:matrix]
   end
 
