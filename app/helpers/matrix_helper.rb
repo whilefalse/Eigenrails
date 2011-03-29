995d11fd88f49126e1ba0a9040603454f9158301
Steven Anderson <steve@whilefalse.net>
2011-03-29 17:22:52 +0100

    Reponing class

--- app/helpers/matrix_helper.rb
+++ app/helpers/matrix_helper.rb
@@ -1,12 +1,12 @@
 require "matrix"
+Matrix.class_eval do
+  def to_tex type='b'
+    '\begin{'+type+'matrix}' + to_a.map {|row| row.join(' & ')}.join(' \\\\') + '\end{'+type+'matrix}'
+  end
+end
+
 module MatrixHelper
   CHART_URL = "https://chart.googleapis.com/chart?cht=tx&chl="
-
-  Matrix.class_eval do
-    def to_tex type='b'
-      '\begin{'+type+'matrix}' + to_a.map {|row| row.join(' & ')}.join(' \\\\') + '\end{'+type+'matrix}'
-    end
-  end
 
   def tex_image tex, alt='Matrix Preview'
     image_tag chart_url(tex), :alt => alt
