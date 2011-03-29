73b75ad26785b626f2508f465db9128f756cab21
Steven Anderson <steve@whilefalse.net>
2011-03-29 09:21:43 +0100

    testing helpers

--- app/controllers/matrices_controller.rb
+++ app/controllers/matrices_controller.rb
@@ -1,6 +1,8 @@
 require "matrix"
 class MatricesController < ApplicationController
 
+  helper MyHelper
+
   def index
     @dimension = 2
     @matrix = Matrix[*params[:matrix].values.map {|row| row.values.map {|elem| elem.to_i}}] if params[:matrix]
