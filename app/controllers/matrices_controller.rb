db73063cbe6c92878277372a6492fbf08e120762
Steven Anderson <steve@whilefalse.net>
2011-03-29 09:33:21 +0100

    dimension 4

--- app/controllers/matrices_controller.rb
+++ app/controllers/matrices_controller.rb
@@ -2,7 +2,7 @@ require "matrix"
 class MatricesController < ApplicationController
 
   def index
-    @dimension = 2
+    @dimension = 4
     @matrix = Matrix[*params[:matrix].values.map {|row| row.values.map {|elem| elem.to_i}}] if params[:matrix]
   end
 
