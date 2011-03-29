5b20dc65c8221cf4fdd01e5f5cc99c835262dfff
Steven Anderson <steve@whilefalse.net>
2011-03-29 15:07:06 +0100

    Links

--- app/controllers/matrices_controller.rb
+++ app/controllers/matrices_controller.rb
@@ -3,7 +3,7 @@ class MatricesController < ApplicationController
 
   def index
     @dimension = if params[:dimension] then params[:dimension].to_i elsif params[:matrix] then params[:matrix].length else 2 end
-    @allowed_dimensions = 2..5
+    @allowed_dimensions = 2..3
     @matrix = Matrix[*params[:matrix].values.map {|row| row.values.map {|elem| elem.to_i}}] if params[:matrix]
   end
 
