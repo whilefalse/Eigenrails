9c583643e8a2091146ff4e11b66092f68e293918
steve <steve@whilefalse.net>
2011-03-31 21:38:40 +0100

    Ok, so monkey patching is bad. I didn't know this

--- app/helpers/matrix_helper.rb
+++ app/helpers/matrix_helper.rb
@@ -1,43 +1,4 @@
 require "matrix"
-Numeric.class_eval do
-  def with_sign
-    (self >= 0 ? "+#{self}" : self).to_s
-  end
-end
-
-Matrix.class_eval do
-  def to_tex type='b'
-    '\begin{'+type+'matrix}' + to_a.map {|row| row.join(' & ')}.join(' \\\\') + '\end{'+type+'matrix}'
-  end
-
-  def eigen_values
-    a, b, c = 1, -trace, determinant
-    rhs = Math.sqrt(b**2 - 4*a*c)
-    [(-b + rhs)/2*a, (-b - rhs) / 2*a]
-  end
-
-  def eigen_values_tex
-    e1, e2 = eigen_values
-    "\\lambda_1 = #{e1.round 3}\\\\ \\lambda_2 = #{e2.round 3}"
-  end
-
-  def char_eq_tex
-    a, b, c = 1, -trace, determinant
-    case row_size
-    when 2 then
-      "\\begin{align}"+
-        "(#{self[0,0]} - \\lambda)(#{self[1,1]} - \\lambda) - (#{self[0,1]})(#{self[1,0]}) &= 0\\\\"+
-        "\\lambda ^2 #{b.with_sign}\\lambda #{c.with_sign} &= 0"+
-        "\\end{align}"
-    else raise "Size too big to calculate eigenvalues"
-    end
-  end
-
-  def to_wolfram
-    '{{' + to_a.map {|row| row.join(',')}.join('},{') + '}}'
-  end
-end
-
 module MatrixHelper
   CHART_URL = "https://chart.googleapis.com/chart?cht=tx&chl="
   WOLFRAM_URL = "http://www.wolframalpha.com/input/?i="
@@ -50,7 +11,38 @@ module MatrixHelper
     CHART_URL + u(tex)
   end
 
-  def wolfram_url code
-    WOLFRAM_URL + u(code)
+  def wolfram_url matrix
+    WOLFRAM_URL + u('{{' + matrix.to_a.map {|row| row.join(',')}.join('},{') + '}}')
+  end
+
+  def matrix_tex matrix, type='b'
+    '\begin{' + type + 'matrix}' + matrix.to_a.map {|row| row.join(' & ')}.join(' \\\\') + '\end{' + type + 'matrix}'
+  end
+
+  def char_eq_tex matrix
+    a, b, c = 1, -matrix.trace, matrix.determinant
+    case matrix.row_size
+    when 2 then
+      "\\begin{align}"+
+        "(#{matrix[0,0]} - \\lambda)(#{matrix[1,1]} - \\lambda) - (#{matrix[0,1]})(#{matrix[1,0]}) &= 0\\\\"+
+        "\\lambda ^2 #{with_sign b}\\lambda #{with_sign c} &= 0"+
+        "\\end{align}"
+    else raise "Size too big to calculate eigenvalues"
+    end
+  end
+
+  def eigen_values matrix
+    a, b, c = 1, -matrix.trace, matrix.determinant
+    rhs = Math.sqrt(b**2 - 4*a*c)
+    [(-b + rhs)/2*a, (-b - rhs) / 2*a]
+  end
+
+  def eigen_values_tex matrix
+    e1, e2 = eigen_values(matrix)
+    "\\lambda_1 = #{e1.round 3}\\\\ \\lambda_2 = #{e2.round 3}"
+  end
+
+  def with_sign x
+    (x >= 0 ? "+#{x}" : x).to_s
   end
 end
--- spec/helpers/matrix_helper_spec.rb
+++ spec/helpers/matrix_helper_spec.rb
@@ -2,35 +2,35 @@ require File.dirname(__FILE__) + '/../spec_helper'
 require "matrix"
 
 describe MatrixHelper do
-  describe 'Fixnum#with_sign' do
+  describe '#with_sign' do
     it "adds a plus sign when zero" do
-      0.with_sign.should == '+0'
+      with_sign(0).should == '+0'
     end
 
     it "adds a plus sign with a positive number" do
-      98.with_sign.should == '+98'
+      with_sign(98).should == '+98'
     end
 
     it "adds a plus sign with a positive float" do
-      0.1.with_sign.should == '+0.1'
+      with_sign(0.1).should == '+0.1'
     end
 
     it "adds a minus sign with a negative number" do
-      (-10).with_sign.should == '-10'
+      with_sign(-10).should == '-10'
     end
 
     it "adds a minus sign with a negative float" do
-      (-0.1).with_sign.should == '-0.1'
+      with_sign(-0.1).should == '-0.1'
     end
   end
 
-  describe 'Matrix#eigen_values' do
+  describe '#eigen_values' do
     it "calculates eigen values for empty matrix" do
-      Matrix[[0,0],[0,0]].eigen_values.should == [0.0, 0.0]
+      eigen_values(Matrix[[0,0],[0,0]]).should == [0.0, 0.0]
     end
 
     it "calculates eign values for integer matrix" do
-      Matrix[[-1,-2],[4,5]].eigen_values.should == [3.0,1.0]
+      eigen_values(Matrix[[-1,-2],[4,5]]).should == [3.0,1.0]
     end
   end
 end
